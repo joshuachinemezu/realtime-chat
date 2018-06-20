d6a687ebe9c7760e0b80400825686dfcd82dd49e
Joshua <joshuachinemezu@gmail.com>
2018-06-20 17:05:50 +0000

    Send user message back to the server

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
-const app = require("express")();
-const server = require("http").Server(app);
-const io = require("socket.io")(server);
+const app = require('express')();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
 });
 
-io.on("connection", socket => {
-  console.log("a user connected");
+io.on('connection', socket => {
+  console.log('a user connected');
+
+  socket.on('chat message', msg => {
+    console.log('message: ' + msg);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
 });
 
 server.listen(4000, () => {
-  console.log("The server is running: http://localhost:4000");
+  console.log('The server is running: http://localhost:4000');
 });
